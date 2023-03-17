9c7d96fbd3899d1340d1c2da33b2091613db541f
zhur <zhur@pie-aael.ru>
2023-03-17 20:02:58 +0700

    Добавлен код для распознавания картинки.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import io
 import streamlit as st
 from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
+
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return EfficientNetB0(weights='imagenet')
+
+
+def preprocess_image(img):
+    img = img.resize((224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    return x
 
 def load_image():
     """Создание формы для загрузки изображения"""
@@ -16,7 +33,23 @@ def load_image():
         return Image.open(io.BytesIO(image_data))
     else:
         return None
-# Выводим заголовок страницы средствами Streamlit     
+
+def print_predictions(preds):
+    classes = decode_predictions(preds, top=3)[0]
+    for cl in classes:
+        st.write(cl[1], cl[2])
+
+
+model = load_model()
+
+# Выводим заголовок страницы средствами Streamlit
 st.title('Классификация изображений')
 # Вызываем функцию создания формы загрузки изображения
 img = load_image()
+
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
