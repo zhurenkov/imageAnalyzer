42e4b9725699842384a4df775f0192c821eed1b5
zhur <zhur@pie-aael.ru>
2023-03-18 01:28:36 +0700

    Изменил импорт tensorflow.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import io
 import streamlit as st
 from PIL import Image
-from tensorflow.keras.applications import EfficientNetB0
-from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.applications.efficientnet import decode_predictions
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import EfficientNetB0
 import numpy as np
 
 
